eb7a351ce752273b5e4c31d185d7cad4b5af8168
IKyzo <99327441+IKyzo@users.noreply.github.com>
2023-06-18 18:32:26 +0100

    Version x
    -

---
+++ chess/draw.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+
+/*
+int main()
+{
+    _setmode(_fileno(stdout), 0x00020000);
+  wprintf (L"\x263a\x2654 Testing ");
+ return 0;
+
+}
+
+*/
+void main()
+{
+    printf("Testing");
+}
